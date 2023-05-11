a036cc951f599d5741b83ecff0e37dcd393983dd
noah0316 <dev.noah0316@gmail.com>
2023-05-12 02:00:12 +0900

    FEAT: FontDimension 구조체 정의 (#1)
    - Font의 size, lineHeight를 나타내는 FontDimension 구조체를 정의하였습니다.

---
+++ iOS/IssueTracker/IssueTracker/Global/Literal/FontStyle.swift
@@ -0,0 +1,18 @@
+//
+//  FontStyle.swift
+//  IssueTracker
+//
+//  Created by Noah on 2023/05/12.
+//
+
+import UIKit.UIFont
+
+enum FontStyle {
+}
+
+private extension FontStyle {
+    struct FontDimension {
+        let size: CGFloat
+        let lineHeight: CGFloat
+    }
+}
