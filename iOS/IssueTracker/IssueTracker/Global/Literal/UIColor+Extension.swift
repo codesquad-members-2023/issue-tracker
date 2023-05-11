a277abb47d777c9f0557554f297ec2dcb7d58f62
demiiann <tkim0156@gmail.com>
2023-05-12 03:28:42 +0900

    FEAT: Extension을 이용하여 UIColor 색상 추가

---
+++ iOS/IssueTracker/IssueTracker/Global/Literal/UIColor+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor+Extension.swift
+//  IssueTracker
+//
+//  Created by ilim on 2023/05/12.
+//
+
+import UIKit
+
+extension UIColor {
+    static let gray50 = UIColor(named: "gray50")
+    static let gray100 = UIColor(named: "gray100")
+    static let gray200 = UIColor(named: "gray200")
+    static let gray300 = UIColor(named: "gray300")
+    static let gray400 = UIColor(named: "gray400")
+    static let gray500 = UIColor(named: "gray500")
+    static let gray600 = UIColor(named: "gray600")
+    static let gray700 = UIColor(named: "gray700")
+    static let gray800 = UIColor(named: "gray800")
+    static let gray900 = UIColor(named: "gray900")
+    static let red = UIColor(named: "red")
+    static let purple = UIColor(named: "purple")
+    static let blue = UIColor(named: "blue")
+}
