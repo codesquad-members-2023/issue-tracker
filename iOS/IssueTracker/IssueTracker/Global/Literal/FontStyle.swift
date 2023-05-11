04010b5406b9371fd9f12e9285acaef3d352138c
noah0316 <dev.noah0316@gmail.com>
2023-05-12 02:01:48 +0900

    FEAT: FontSize 열거형 정의 (#1)
    - 디자인이 정의되어있는 Figma를 참고하여 FontSize에 따른 열거형을 정의하였습니다.

--- iOS/IssueTracker/IssueTracker/Global/Literal/FontStyle.swift
+++ iOS/IssueTracker/IssueTracker/Global/Literal/FontStyle.swift
@@ -8,6 +8,14 @@
 import UIKit.UIFont
 
 enum FontStyle {
+private extension FontStyle {
+    enum FontSize {
+        static let doubleXLarge: FontDimension = .init(size: 32, lineHeight: 48)
+        static let xLarge: FontDimension = .init(size: 24, lineHeight: 40)
+        static let large: FontDimension = .init(size: 18, lineHeight: 32)
+        static let medium: FontDimension = .init(size: 15, lineHeight: 24)
+        static let small: FontDimension = .init(size: 12, lineHeight: 16)
+    }
 }
 
 private extension FontStyle {
