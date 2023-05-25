eed6d0ffb110682a259e25bc76e5d026f2aa846b
JaydenLee1116 <vv55adss@gmail.com>
2023-05-25 18:43:05 +0900

    feat #84: 이슈 content(첫번째 comment) 추가

--- fe/src/components/IssueCommentItem/IssueCommentItem.tsx
+++ fe/src/components/IssueCommentItem/IssueCommentItem.tsx
@@ -11,9 +11,6 @@ interface IssueCommentItemProps {
   isWriterComment: boolean;
 }
 
-const TEMP_PROFILE_URL =
-  'https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80';
-
 const IssueCommentItem = (props: IssueCommentItemProps) => {
   const { comment, isWriterComment } = props;
   const { days, hours, minutes, seconds } = getTimeElapsed(comment.createdAt);
--- fe/src/components/IssueCommentList/IssueCommentList.tsx
+++ fe/src/components/IssueCommentList/IssueCommentList.tsx
@@ -10,14 +10,24 @@ interface CommentListProps {
 
 const IssueCommentList = (props: CommentListProps) => {
   const { comments, issue } = props;
-  const { userName, profileUrl } = issue;
+  // FIXME(Jayden): 추후 작성자 content(첫번째 comment)에 관한 로직 수정하기
+  const writerComment = {
+    commentId: issue.issueId,
+    userName: issue.userName,
+    content: issue.content,
+    createdAt: issue.createdAt,
+    profileUrl: issue.profileUrl,
+    updateAt: '',
+    userId: 0,
+  };
+  const totalComments = [writerComment, ...comments];
   return (
     <ul className="flex h-full flex-col justify-between gap-6">
-      {comments.map(comment => (
+      {totalComments.map(comment => (
         <IssueCommentItem
           key={comment.commentId}
           comment={comment}
-          isWriterComment={comment.userName === userName}
+          isWriterComment={comment.userName === issue.userName}
         />
       ))}
     </ul>
