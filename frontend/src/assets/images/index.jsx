7f5ff643553042f2fda30969a112b034e12f43ea
saehim <saeilee.vxv@gmail.com>
2023-06-02 12:11:58 +0900

    Remove: 충돌 방지를 위해 frontend 파일 삭제

--- frontend/src/assets/images/index.jsx
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as logoType } from './Logotype.svg';
-import { ReactComponent as logoTypeDarkMode } from './LogotypeDarkMode.svg';
-import { ReactComponent as userImage } from './userImage.svg';
-
-export default {
-  logoType,
-  logoTypeDarkMode,
-  userImage,
-};
--- frontend/src/services/milestone.js
+++
@@ -1,9 +0,0 @@
-import { customFetch } from './api';
-
-export const getMilestone = async () => {
-  const response = await customFetch({
-    path: `/milestones`,
-    method: 'GET',
-  });
-  return response;
-};
